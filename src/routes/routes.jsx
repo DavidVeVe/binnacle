cfe7cc24ef71f552c41dd7e08511a7377900fa6e
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2025-01-19 19:18:44 -0600

    Add routes and layout components

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,6 @@
 import Home from "../layouts/Home/Home";
+import DailyRecord from "../layouts/DailyRecord/DailyRecord";
+import CashOut from "../layouts/Cashout/Cashout";
 
 export const routes = Object.freeze([
   {
@@ -6,5 +8,17 @@ export const routes = Object.freeze([
     pageName: "Home",
     title: "Home",
     component: <Home />
+  },
+  {
+    path: "/profile/:profileId",
+    pageName: "Profile",
+    title: "Profile",
+    component: <DailyRecord />
+  },
+  {
+    path: "/profile/:profileId/cash-out",
+    pageName: "Cash out",
+    title: "Cash out",
+    component: <CashOut />
   }
 ]);
