6aa118a498922bbe9348622fe6840466880141ad
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2025-01-20 15:12:50 -0600

    Add basic title component

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -1,4 +1,7 @@
-export default function Text() {
-    return <p>Text</p>;
-  }
-  
+import { defaultTitleSize, validTags } from "./helper";
+
+export default function Text({ children, element }) {
+  let TextComponent = validTags[element] || defaultTitleSize;
+
+  return <TextComponent className={"title"}>{children}</TextComponent>;
+}
