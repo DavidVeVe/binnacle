e313c129930b494355b061befc0c7eb3ecff539a
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2025-01-20 14:34:59 -0600

    Add basic avatar component

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,12 +1,15 @@
 import AvatarImage from "../../assets/woman.png";
+import { Link } from "react-router";
 import "./avatar.scss";
 
-export default function Avatar({ onClick }) {
+export default function Avatar({ onClick, profileId }) {
   return (
-    <div onClick={onClick} className="avatar">
-      <figure className="avatar__image-wrapper">
-        <img src={AvatarImage} />
-      </figure>
-    </div>
+    <Link to={`/profile/${profileId}`}>
+      <div onClick={onClick} className="avatar">
+        <figure className="avatar__image-wrapper">
+          <img src={AvatarImage} />
+        </figure>
+      </div>
+    </Link>
   );
 }
--- src/components/layouts/Home/Home.jsx
+++ src/components/layouts/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <section>
       <article>
-        <Avatar onClick={avatarClickHandler} />
+        <Avatar onClick={avatarClickHandler} profileId="Abril" />
       </article>
     </section>
   );
